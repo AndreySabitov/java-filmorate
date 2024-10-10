18d073f57c36894d6ac33cf477004b45ffbc20cf
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2024-10-10 16:43:46 +0500

    Убрать заполнение таблиц genre и mpa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,2 @@
 INSERT INTO FRIENDSHIP_STATUS (status_name)
 VALUES ('ACCEPTED'), ('REQUESTED');
-
-INSERT INTO rating (rating_name)
-VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
-
-INSERT INTO genre (genre_name)
-VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
