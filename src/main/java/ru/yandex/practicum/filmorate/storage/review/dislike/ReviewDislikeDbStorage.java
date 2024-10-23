bd93764c3b3edbd34b1b83e9861c9a052f56264e
Pancake <nik.moskovenko@yandex.ru>
2024-10-23 19:43:37 +0300

    править поля

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/dislike/ReviewDislikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/dislike/ReviewDislikeDbStorage.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class ReviewDislikeDbStorage implements ReviewDislikeStorage{
 
-    private static JdbcTemplate jdbc;
+    private final JdbcTemplate jdbc;
 
     @Override
     public void addReviewDislike(Integer reviewId, Integer userId) {
