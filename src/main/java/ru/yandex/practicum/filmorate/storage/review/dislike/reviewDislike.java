01dc9c04153b7fc43f820c8179a79b2c58bea440
Pancake <nik.moskovenko@yandex.ru>
2024-10-23 14:15:21 +0300

    добавить класс reviewDislike

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/dislike/reviewDislike.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.storage.review.dislike;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class reviewDislike {
+    @NotNull
+    Integer filmId;
+    @NotNull
+    Integer userId;
+}
