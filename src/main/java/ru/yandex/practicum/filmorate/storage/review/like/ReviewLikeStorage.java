cfcc1e287ea772d1d61316abaa7dc2cf0f31fbd3
Pancake <nik.moskovenko@yandex.ru>
2024-10-23 18:43:48 +0300

    Убрать лишние импорты

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/like/ReviewLikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/like/ReviewLikeStorage.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.review.like;
 
-import java.util.List;
-
 public interface ReviewLikeStorage {
     
     void addReviewLike(Integer reviewId, Integer userId);
