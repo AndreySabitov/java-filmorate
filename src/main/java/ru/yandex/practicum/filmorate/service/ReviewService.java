0ddead6b573720925c4cb14e5bf3f99fc8c9afeb
Pancake <nik.moskovenko@yandex.ru>
2024-10-23 20:10:33 +0300

    добавить класс ReviewService

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -0,0 +1,58 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.storage.review.ReviewStorage;
+import ru.yandex.practicum.filmorate.storage.review.dislike.ReviewDislikeStorage;
+import ru.yandex.practicum.filmorate.storage.review.like.ReviewLikeStorage;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewStorage reviewStorage;
+    private final ReviewLikeStorage reviewLikeStorage;
+    private final ReviewDislikeStorage reviewDislikeStorage;
+
+    public Review addNewReview(Review newReview) {
+        Review addedReview = reviewStorage.addNewReview(newReview);
+        try {
+            return reviewStorage.getReviewById(addedReview.getReviewId());
+        } catch (Exception e) {
+            throw new NotFoundException("Указанный фильм или пользователь не существуют");
+        }
+    }
+
+    public Review updateReview(Review newReview) {
+        return reviewStorage.updateReview(newReview);
+    }
+
+    public void removeReview(Integer id) {
+        reviewStorage.removeReview(id);
+    }
+
+    public List<Review> getAllReviewsByFilmId(Integer filmId, int count) {
+        return reviewStorage.getAllReviewsByFilmId(filmId, count);
+    }
+
+    public void addReviewLike(Integer reviewId, Integer userId) {
+        reviewLikeStorage.addReviewLike(reviewId, userId);
+    }
+
+    public void removeReviewLike(Integer reviewId, Integer userId) {
+        reviewLikeStorage.removeReviewLike(reviewId, userId);
+    }
+
+    public void addReviewDislike(Integer reviewId, Integer userId) {
+        reviewDislikeStorage.addReviewDislike(reviewId, userId);
+    }
+
+    public void removeReviewDislike(Integer reviewId, Integer userId) {
+        reviewDislikeStorage.removeReviewDislike(reviewId, userId);
+    }
+}
