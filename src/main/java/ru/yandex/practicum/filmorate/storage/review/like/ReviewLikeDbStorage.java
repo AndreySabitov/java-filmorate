0a70f1a867abe9528c4af86065aaac83e5ad0941
Pancake <nik.moskovenko@yandex.ru>
2024-10-23 19:12:50 +0300

    добавить классы работы с лайками и дизлайками отзыва

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/like/ReviewLikeDbStorage.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorate.storage.review.like;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.InternalServerException;
+
+@Primary
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ReviewLikeDbStorage implements ReviewLikeStorage{
+
+    private static JdbcTemplate jdbc;
+
+    @Override
+    public void addReviewLike(Integer reviewId, Integer userId) {
+        final String insert = "INSERT INTO review_likes (review_id, user_id) VALUES (?, ?)";
+        final String increaseUsefulness = "UPDATE reviews SET usefulness_rate = usefulness_rate + 1 WHERE id = ?";
+        String message = "Ошибка добавления лайка к отзыву";
+        boolean isSuccess = updater(insert, increaseUsefulness, reviewId, userId);
+        if (!isSuccess) {
+            log.error(message);
+        }
+    }
+
+    @Override
+    public void removeReviewLike(Integer reviewId, Integer userId) {
+        final String delete = "DELETE FROM review_likes WHERE review_id=? AND user_id=?";
+        final String decreaseUsefulness = "UPDATE reviews SET usefulness_rate = usefulness_rate - 1 WHERE id = ?";
+        String message = "Ошибка удаления лайка к отзыву";
+        boolean isSuccess = updater(delete, decreaseUsefulness, reviewId, userId);
+        if (!isSuccess) {
+            log.error(message);
+        }
+    }
+
+    private boolean updater(String firstQuery, String secondQuery, Integer reviewId, Integer userId) {
+        jdbc.update(firstQuery, reviewId, userId);
+        jdbc.update(secondQuery, reviewId);
+        return ((jdbc.update(firstQuery, reviewId, userId) > 0) && (jdbc.update(secondQuery, reviewId) > 0));
+    }
+}
